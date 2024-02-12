ca6d53b0866ec3952888f636141efd862566cbf5
AlkoKovad <aleksandr.kopeykin@gmail.com>
2024-02-12 22:50:53 +0300

    header

--- Core/settings.py
+++ Core/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Includes files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/Static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
